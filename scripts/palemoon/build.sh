3174d7b4a7815af95b98a471016593a565bf543a
Vaguiner Gonzalez <cardealrusso@vaguiner.eu.org>
2025-07-20 23:50:38 +0000

    Update build.sh

--- scripts/palemoon/build.sh
+++ scripts/palemoon/build.sh
@@ -37,8 +37,10 @@ export CPPFLAGS="-fopt-info-vec-optimized -fmerge-all-constants -fno-semantic-in
 export CXXFLAGS="-fopt-info-vec-optimized -fmerge-all-constants -fno-semantic-interposition -ftree-vectorize -fipa-pta -funroll-loops -floop-nest-optimize -O3 -march=$MARCH"
 export LDFLAGS="-Wl,-O2,--as-needed,--sort-common -flto -fuse-linker-plugin"
 
-tce-load -lwi python compiletc Xorg-7.7-3d-dev gtk3-dev yasm python-dev coreutils binutils zip perl5 alsa-dev ffmpeg7-dev clang xz
+tce-load -lwi python compiletc Xorg-7.7-3d-dev gtk3-dev yasm python-dev coreutils binutils zip perl5 alsa-dev ffmpeg7-dev clang xz tar
 sudo rm -rf /usr/bin/xz
+sudo rm -rf /bin/tar
+sudo cp /usr/local/bin/tar /bin/tar
 sudo cp /usr/local/bin/xz /bin/xz
 
 #Fix broken clang
