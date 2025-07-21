cf1cb9fd6a637b24a2ff0d1ee26def1f7dcd1901
Vaguiner Gonzalez <cardealrusso@vaguiner.eu.org>
2025-07-21 08:10:01 +0000

    Update build.sh

--- scripts/openttd/build.sh
+++ scripts/openttd/build.sh
@@ -6,6 +6,11 @@ if [ -z "$MARCH" ]; then
   exit 1
 fi
 
+if [ "$MARCH" = "x86-64-v4" ]; then
+  echo "Error: not working"
+  exit 1
+fi
+
 echo "Building with -march=$MARCH"
 
 workdir=$(mktemp -d)
@@ -32,7 +37,7 @@ case "$1" in
 esac' | sudo tee /bin/uname > /dev/null
 sudo chmod +x /bin/uname
 
-tce-load -lwi sdl2-dev git compiletc cmake curl-dev tce-load fontconfig-dev freetype-dev Xorg-7.7-3d-dev liblzma-dev opus-dev mksquashfs-tools submitqc
+tce-load -lwi sdl2-dev git compiletc cmake curl-dev tce-load fontconfig-dev freetype-dev Xorg-7.7-3d-dev liblzma-dev opus-dev mksquashfs-tools submitqc sstrip
 export LDFLAGS="-Wl,-O2,--as-needed,--sort-common -flto -fuse-linker-plugin"
 export CFLAGS="-fopt-info-vec-optimized -fmerge-all-constants -fno-semantic-interposition -ftree-vectorize -fipa-pta -funroll-loops -floop-nest-optimize -Ofast -march=$MARCH -flto"
 export CXXFLAGS="-fopt-info-vec-optimized -fmerge-all-constants -fno-semantic-interposition -ftree-vectorize -fipa-pta -funroll-loops -floop-nest-optimize -Ofast -march=$MARCH -flto"
@@ -40,10 +45,16 @@ export CXXFLAGS="-fopt-info-vec-optimized -fmerge-all-constants -fno-semantic-in
 git clone --recursive https://github.com/OpenTTD/OpenTTD
 cd OpenTTD
 
-cmake -DCMAKE_BUILD_TYPE=MinSizeRel -B build .
+cmake -DCMAKE_BUILD_TYPE=Release -B build .
 cd build
 make -j4
 sudo make install DESTDIR=/tmp/openttd
+
+sstrip -z /tmp/openttd/usr/local/games/openttd
+mkdir -p /tmp/openttd/usr/local/bin
+rm -rf /tmp/openttd/usr/local/games
+mv /tmp/openttd/usr/local/games/openttd /tmp/openttd/usr/local/bin
 mksquashfs /tmp/openttd openttd.tcz
+
 sudo submitqc --nonet --blocksize=65536 openttd.tcz
 mv -f openttd.tcz /output/
