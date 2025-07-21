a28a5c22403d0fe5fc84cd5fe74982342eca6922
Vaguiner Gonzalez <cardealrusso@vaguiner.eu.org>
2025-07-21 17:50:10 +0000

    Update build.sh

--- scripts/openttd/build.sh
+++ scripts/openttd/build.sh
@@ -37,7 +37,7 @@ case "$1" in
 esac' | sudo tee /bin/uname > /dev/null
 sudo chmod +x /bin/uname
 
-tce-load -lwi sdl2-dev git compiletc cmake curl-dev tce-load fontconfig-dev freetype-dev Xorg-7.7-3d-dev liblzma-dev opus-dev squashfs-tools sstrip submitqc
+tce-load -lwi sdl2-dev git compiletc cmake curl-dev tce-load fontconfig-dev freetype-dev Xorg-7.7-3d-dev liblzma-dev opus-dev squashfs-tools submitqc
 export LDFLAGS="-Wl,-O2,--as-needed,--sort-common -flto -fuse-linker-plugin"
 export CFLAGS="-fopt-info-vec-optimized -fmerge-all-constants -fno-semantic-interposition -ftree-vectorize -fipa-pta -funroll-loops -floop-nest-optimize -Ofast -march=$MARCH -flto"
 export CXXFLAGS="-fopt-info-vec-optimized -fmerge-all-constants -fno-semantic-interposition -ftree-vectorize -fipa-pta -funroll-loops -floop-nest-optimize -Ofast -march=$MARCH -flto"
@@ -51,7 +51,6 @@ make -j4
 make install DESTDIR=/tmp/openttd
 
 find /tmp/openttd
-sstrip -z /tmp/openttd/usr/local/games/openttd
 strip -s /tmp/openttd/usr/local/games/openttd
 mkdir -p /tmp/openttd/usr/local/bin
 mv /tmp/openttd/usr/local/games/openttd /tmp/openttd/usr/local/bin
