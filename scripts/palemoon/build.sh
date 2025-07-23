11202326818cfe4f6324f5a063cc93bb14ed0411
Vaguiner Gonzalez <cardealrusso@vaguiner.eu.org>
2025-07-23 00:27:19 +0000

    Update build.sh

--- scripts/palemoon/build.sh
+++ scripts/palemoon/build.sh
@@ -32,8 +32,7 @@ workdir=$(mktemp -d)
 cp /scripts/mozconfig $workdir
 cd $workdir
 
-#Cannot build libopus with -ffast-math unless FLOAT_APPROX is defined.
-#export LDFLAGS="-Wl,-O2,--as-needed,--sort-common -flto -fuse-linker-plugin"
+export LDFLAGS="-Wl,-O2,--as-needed,--sort-common -flto -fuse-linker-plugin"
 
 tce-load -lwi python compiletc Xorg-7.7-3d-dev gtk3-dev yasm python-dev coreutils binutils zip perl5 alsa-dev ffmpeg7-dev clang xz tar \
  squashfs-tools node sstrip gstreamer cairo-dev cairomm-dev pixman-dev submitqc
